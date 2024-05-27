d1ac21d3d7ad3214ab357cd0bc40b1899a4c442a
Ifeanyi <ifeanyiwisdom25@yahoo.com>
2024-05-27 10:54:29 +0200

    first commit

---
+++ hellomaven/src/main/java/com/ionyejekwe/App.java
@@ -0,0 +1,13 @@
+package com.ionyejekwe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
